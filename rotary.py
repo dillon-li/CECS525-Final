5aac55e98984fdb8a92fd220d03a5058d8b14213
unknown <green.koopa667@gmail.com>
2017-12-04 03:02:47 -0500

    Potentially working script for getting key input on rotations.

---
+++ rotary.py
@@ -0,0 +1,51 @@
+from RPI import GPIO
+from time import sleep
+import uinput
+
+# Define our "keyboard"
+device = uinput.Device([
+    uinput.KEY_A,
+    uninput.KEY_Y
+    ])
+
+# Define GPIO pin for our CLK pin of rotary encoder
+clk = 17
+
+# Define GPIO pin for our DT pin of rotary encoder
+dt = 18
+
+# Set pins to inputs with pulldown resistor
+GPIO.setmode(GPIO,BCM)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Initial state setup
+clkLastState = GPIO.input(clk)
+
+# Listen for rotations and do shit
+try:
+    while True:
+        # Get current states
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+
+        # Check for rotations
+        if clkState != clkLastState:
+            if dtState != clkState:
+                print("We clockwise bois")
+                sleep(.01)
+                device.emit_click(uinput.KEY_A)
+                device.emit_click(uinput.KEY_Y)
+                device.emit_click(uinput.KEY_Y)
+            else:
+                print("We counterclockwise bois")
+                sleep(.01)
+                device.emit_click(uinput.KEY_Y)
+                device.emit_click(uinput.KEY_Y)
+                device.emit_click(uinput.KEY_A)
+        clkLastState = clkState # set for next rotation
+        sleep(0.01) # Put some slight waiting in there
+
+# Not entirely sure why this is here, but it looks important
+finally:
+    GPIO.cleanup()
