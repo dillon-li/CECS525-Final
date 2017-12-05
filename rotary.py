2f50c5239449ba8338c3538995fcb3d53f33ebef
Dillon Li <green.koopa667@gmail.com>
2017-12-05 14:14:06 -0500

    Added more buttons. Buttons seem to work now. xD

--- rotary.py
+++ rotary.py
@@ -5,7 +5,12 @@ import uinput
 # Define our "keyboard"
 device = uinput.Device([
     uinput.KEY_A,
-    uinput.KEY_Y
+    uinput.KEY_Y,
+    uinput.KEY_H,
+    uinput.KEY_X,
+    uinput.KEY_D,
+    uinput.KEY_B,
+    uinput.KEY_T
     ])
 
 # Define GPIO pin for our CLK pin of rotary encoder
@@ -14,10 +19,17 @@ clk = 11
 # Define GPIO pin for our DT pin of rotary encoder
 dt = 12
 
+button1 = 16
+button2 = 15
+button3 = 18
+
 # Set pins to inputs with pulldown resistor
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Initial state setup
 clkLastState = GPIO.input(clk)
@@ -28,8 +40,25 @@ try:
         # Get current states
         clkState = GPIO.input(clk)
         dtState = GPIO.input(dt)
-
+        b1State = GPIO.input(button1)
+        b2State = GPIO.input(button2)
+        b3State = GPIO.input(button3)
         # Check for rotations
+        # print b1State
+        #if not b1State:
+            #device.emit_click(uinput.KEY_H)
+        if not b1State:
+            device.emit_click(uinput.KEY_X)
+            device.emit_click(uinput.KEY_D)
+            sleep(0.2)
+        if not b2State:
+            device.emit_click(uinput.KEY_H)
+            device.emit_click(uinput.KEY_H)
+            sleep(0.2)
+        if not b3State:
+            device.emit_click(uinput.KEY_B)
+            device.emit_click(uinput.KEY_T)
+            sleep(0.2)
         if clkState != clkLastState:
             if dtState != clkState:
                 print("We clockwise bois")
